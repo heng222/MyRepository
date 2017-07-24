c3187f7be4bbe13866974ee49e6a6d967ad6d30a
heng222_z <heng222_z@163.com>
2017-07-24 15:24:19 +0800

    modified   Hello World/CSharpLearning/Collections/BitArrayTest.cs

--- C#/Hello World/CSharpLearning/Collections/BitArrayTest.cs
+++ C#/Hello World/CSharpLearning/Collections/BitArrayTest.cs
@@ -13,20 +13,39 @@ namespace CSharpLearning.Collections
         [Test]
         public void Test1()
         {
-            var bytes = new byte[] { 0xAA };
+            var bytes = new byte[] { 0xAA, 0x21,  0x13};
 
             var bitArray = new BitArray(bytes);
 
-
+            // 0xAA = 1010 1010
             Assert.IsFalse(bitArray.Get(0));
             Assert.IsTrue(bitArray.Get(1));
             Assert.IsFalse(bitArray.Get(2));
             Assert.IsTrue(bitArray.Get(3));
-
             Assert.IsFalse(bitArray.Get(4));
             Assert.IsTrue(bitArray.Get(5));
             Assert.IsFalse(bitArray.Get(6));
             Assert.IsTrue(bitArray.Get(7));
+
+            // 0x21 = 0010 0001
+            Assert.IsTrue(bitArray[8]);
+            Assert.IsFalse(bitArray[9]);
+            Assert.IsFalse(bitArray[10]);
+            Assert.IsFalse(bitArray[11]);
+            Assert.IsFalse(bitArray[12]);
+            Assert.IsTrue(bitArray[13]);
+            Assert.IsFalse(bitArray[14]);
+            Assert.IsFalse(bitArray[15]);
+
+            // 0x13 = 0001 0011
+            Assert.IsTrue(bitArray[16]);
+            Assert.IsTrue(bitArray[17]);
+            Assert.IsFalse(bitArray[18]);
+            Assert.IsFalse(bitArray[19]);
+            Assert.IsTrue(bitArray[20]);
+            Assert.IsFalse(bitArray[21]);
+            Assert.IsFalse(bitArray[22]);
+            Assert.IsFalse(bitArray[23]);
         }
 
         [Test(Description="将BOOL型数组转换成字节流。")]
@@ -55,6 +74,25 @@ namespace CSharpLearning.Collections
             bitArray.CopyTo(bytes, 0);
             Assert.AreEqual(0xAA, bytes[0]);
             Assert.AreEqual(0x55, bytes[1]);
+        }
+
+        [Test(Description = "CI码位解析测试。")]
+        public void Test11()
+        {
+            // 假设一组（1个道岔、一个信号机）码位需要22个比特。
+            // 道岔四开在第3（索引号为2）个比特。
+            // 假设2个道岔，2个信号机，则至少需要22*2 = 44比特，取整即6个字节。
+
+            var bytes = new byte[] { 0x04, 0x00, 0x00, 0x01, 0x00, 0x00, /* 之前是必须的字节 */
+                                                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+            var bitArray = new BitArray(bytes);
+
+            // 第一个道岔是否四开？
+            Assert.IsTrue(bitArray.Get(0 * 22 + 2));
+
+            // 第二个道岔是否四开？
+            Assert.IsTrue(bitArray.Get(1 * 22 + 2));
         }
     }
 }
